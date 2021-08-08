05e64e9742d70dd02969d71243ddcf862c89654f
yuuki <yuki.tsubo@gmail.com>
2021-08-08 09:42:34 +0900

    tsdr: use argparser choices

--- tsdr/tsdr.py
+++ tsdr/tsdr.py
@@ -352,12 +352,25 @@ def aggregate_dimension(data_df):
     return metrics_dimension
 
 
+def check_cause_metrics(metrics: list[str], chaos_type: str, chaos_comp: str
+                        ) -> Tuple[bool, list[Any]]:
+    prefix = CHAOS_TO_CAUSE_METRIC_PREFIX[chaos_type]
+    cause_metrics = []
+    for metric in metrics:
+        if re.match(f"^c-{chaos_comp}_{prefix}.+", metric):
+            cause_metrics.append(metric)
+    if len(cause_metrics) > 0:
+        return True, cause_metrics
+    return False, cause_metrics
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("datafile", help="metrics JSON data file")
     parser.add_argument("--method",
+                        choices=['tsifter', 'sieve'],
                         help="specify one of tsdr methods",
-                        type=str, default=TSIFTER_METHOD)
+                        default=TSIFTER_METHOD)
     parser.add_argument("--max-workers",
                         help="number of processes",
                         type=int, default=1)
