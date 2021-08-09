08d415068d1bd1d7c3eb3e1ed6e16cfec843fda7
yuuki <yuki.tsubo@gmail.com>
2021-08-09 15:25:06 +0900

    tools: add reduction performance and execution time

--- tools/tsdr_combine_params.py
+++ tools/tsdr_combine_params.py
@@ -2,7 +2,9 @@
 
 import argparse
 import json
+import logging
 import sys
+from collections import defaultdict
 from multiprocessing import cpu_count
 
 from lib.metrics import check_cause_metrics
@@ -12,19 +14,27 @@ DIST_THRESHOLDS = [0.001, 0.01, 0.1]
 
 
 def main():
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("metricsfiles",
                         nargs='+',
                         help="metrics output JSON file")
     args = parser.parse_args()
 
-    summary = {}
+    results = defaultdict(lambda: defaultdict(
+        lambda: defaultdict(dict)
+    ))
     for metrics_file in args.metricsfiles:
         data_df, _, metrics_meta = tsdr.read_metrics_json(metrics_file)
         chaos_type: str = metrics_meta['injected_chaos_type']
         chaos_comp: str = metrics_meta['chaos_injected_component']
         for thresh in DIST_THRESHOLDS:
-            _, reduced_df, _, _ = tsdr.run_tsdr(
+            key = f"{chaos_type}:{chaos_comp}"
+
+            logging.info(f">> Running tsdr {metrics_file} [{key}] dict_threshold:{thresh} ...")
+
+            elapsedTime, reduced_df, _, _ = tsdr.run_tsdr(
                 data_df=data_df,
                 method=tsdr.TSIFTER_METHOD,
                 max_workers=cpu_count(),
@@ -36,11 +46,11 @@ def main():
                 chaos_type=chaos_type,
                 chaos_comp=chaos_comp,
             )
-            key = f"{chaos_type}:{chaos_comp}"
-            summary.setdefault(key, {})
-            summary[key][thresh] = ok
+            results[key]['params']['dict_threshold'][thresh] = ok
+            results[key]['reduction_performance']['reduced_series_num'] = len(reduced_df.columns)
+            results[key]['execution_time'] = round(elapsedTime['step1'] + elapsedTime['step2'], 2)
 
-    json.dump(summary, sys.stdout, indent=4)
+    json.dump(results, sys.stdout, indent=4)
 
 
 if __name__ == '__main__':
