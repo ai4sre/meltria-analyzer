b8def767ccd6ca9569da8c5401c44fd8ecbce25a
yuuki <yuki.tsubo@gmail.com>
2022-02-02 11:28:04 +0900

    tests: fix that the test code didn't compare want and got

--- tests/tsdr/outlierdetection/test_ar.py
+++ tests/tsdr/outlierdetection/test_ar.py
@@ -5,11 +5,11 @@ from tsdr.outlierdetection.ar import AROutlierDetector
 def test_ar_outlier_detector_score():
     ar = AROutlierDetector()
     input = np.array([1, 2, 10, 2, 1, 0.5, 0.8, 1.5, 5.0, 4.8, 7.2, 9.0, 12.1])
-    assert ar.score(input), [np.nan, 0.08986889267696001, 3.7183484546650334, 3.8075135043425483, 0.6158008427697705, 0.5654338656623322, 0.32463645172583305, 0.17123483665528313, 0.26297645945662845, 0.03599698801645843, 0.3254092017134403, 0.4524605268734048, 1.6303199754423074]
+    assert ar.score(input)[1:] == [np.nan, 0.08986889267696001, 3.7183484546650334, 3.8075135043425483, 0.6158008427697705, 0.5654338656623322, 0.32463645172583305, 0.17123483665528313, 0.26297645945662845, 0.03599698801645843, 0.3254092017134403, 0.4524605268734048, 1.6303199754423074][1:]
 
 
 def test_ar_outlier_detector_find_anomalies():
     ar = AROutlierDetector()
     input = np.array([1, 2, 10, 2, 1, 0.5, 0.8, 1.5, 5.0, 4.8, 7.2, 9.0, 12.1])
     anomalies = ar.find_anomalies(input, 1.5)
-    assert len(anomalies), 3
+    assert len(anomalies) == 3
--- tests/tsdr/outlierdetection/test_knn.py
+++ tests/tsdr/outlierdetection/test_knn.py
@@ -5,11 +5,11 @@ from tsdr.outlierdetection.knn import KNNOutlierDetector
 def test_knn_outlier_detector_score():
     knn = KNNOutlierDetector(w=2, k=1)
     input = np.array([1, 2, 10, 2, 1])
-    assert knn.score(input), [np.nan, 1.4142135623730951, 8.06225774829855, 8.06225774829855, 1.4142135623730951]
+    assert knn.score(input)[1:] == [np.nan, 1.4142135623730951, 8.06225774829855, 8.06225774829855, 1.4142135623730951][1:]
 
 
 def test_knn_outlier_detector_find_anomalies():
     knn = KNNOutlierDetector(w=2, k=1)
     input = np.array([1, 2, 10, 2, 1])
     anomalies = knn.find_anomalies(input, 3)
-    assert len(anomalies), 1
+    assert len(anomalies) == 2
