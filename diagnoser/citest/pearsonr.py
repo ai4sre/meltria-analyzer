44fd1f9c4536f584533d0bffe76da4299737c021
yuuki <yuki.tsubo@gmail.com>
2022-03-24 16:39:30 +0900

    diag: fix ci_test_pearsonr

--- diagnoser/citest/pearsonr.py
+++ diagnoser/citest/pearsonr.py
@@ -2,17 +2,17 @@ import numpy as np
 from scipy import stats
 
 
-def ci_test_pearsonr(data_matrix: np.ndarray, x: int, y: int, s: set[int], **kwargs):
+def ci_test_pearsonr(dm: np.ndarray, x: int, y: int, s: set[int], **kwargs):
     assert 'corr_matrix' in kwargs
-    cm: np.ndarray = kwargs['corr_matrix']
+    # cm: np.ndarray = kwargs['corr_matrix']
     s = list(s)
     if len(s) == 0:
-        coef, p_val = stats.pearsonr(cm[x], cm[y])
+        coef, p_val = stats.pearsonr(dm[:, x], dm[:, y])
     else:
-        X_coef = np.linalg.lstsq(cm[:, s], cm[:, x], rcond=None)[0]
-        Y_coef = np.linalg.lstsq(cm[:, s], cm[:, y], rcond=None)[0]
+        X_coef = np.linalg.lstsq(dm[:, s], dm[:, x], rcond=None)[0]
+        Y_coef = np.linalg.lstsq(dm[:, s], dm[:, y], rcond=None)[0]
 
-        res_X = cm[:, x] - cm[:, s].dot(X_coef)
-        res_Y = cm[:, y] - cm[:, s].dot(Y_coef)
+        res_X = dm[:, x] - dm[:, s].dot(X_coef)
+        res_Y = dm[:, y] - dm[:, s].dot(Y_coef)
         coef, p_val = stats.pearsonr(res_X, res_Y)
     return p_val
